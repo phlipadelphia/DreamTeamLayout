2b745c9be17e3a766141375016a6cf4d9b3217c3
Zephyr <phillip.t.mock@gmail.com>
2022-06-14 15:58:03 -0700

    first commit, start code files and folders

---
+++ index.js
@@ -0,0 +1,6 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
