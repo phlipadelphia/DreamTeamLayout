7b7cbd1dbd84df418479abd15813d1a778610b9a
Zephyr <phillip.t.mock@gmail.com>
2022-06-14 16:11:11 -0700

    more files starter code indexjs

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+// creating link to the html generated
+const generateHTML = require('./src/generateHTML');
+// const team profiles and node modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Manager");
 const Intern = require("./lib/Intern");
+// const node modules and team
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+const teamArray = [];
+addToTeam();
+
+function addToTeam() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "addEmployee",
+            message: "Add an employee, or select 'Complete'.",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+                "Complete"
+            ]
+        }
+    ]).then(function(data){
+        const employeeRole = data.addEmployee;
+        if (employeeRole === "Manager") {
+            managerData();
+        } else if (employeeRole === "Engineer") {
+            engineerData();
+        } else if (employeeRole === "Intern") {
+            internData(); 
+        } else if (employeeRole === "Complete") {
+            renderTeam();
+        }
+    });
+}
